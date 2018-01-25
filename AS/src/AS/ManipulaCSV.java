3868e89be4816b1a3fe14341e64e531039f94ca9
Diego <Diego@COMP-Diego>
2018-01-25 20:58:16 -0300

    Comparaçaõ com GS, cálculo de métricas e geração de arquivos OK

--- AS/src/AS/ManipulaCSV.java
+++ AS/src/AS/ManipulaCSV.java
@@ -31,17 +31,13 @@ public class ManipulaCSV {
         estat = new File("./src/csv/", "estatisticas.csv");
 
         if (!estat.exists()) {
+            System.out.println("Não existe arquivo estatisticas.csv.");
+
             try {
                 estat.createNewFile();
-
-            } catch (IOException ex) {
-                Logger.getLogger(ManipulaCSV.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-
-        try {
-            estatisticas = new FileWriter(estat);
-            BufferedWriter StrW = new BufferedWriter(estatisticas);
+                try {
+                    estatisticas = new FileWriter(estat, true);
+                    BufferedWriter StrW = new BufferedWriter(estatisticas);
 
 //            StrW.append("Precision");
 //            StrW.append(";");
@@ -51,9 +47,17 @@ public class ManipulaCSV {
 //            StrW.append(";");
 //            StrW.append("Iteracao");
 //            StrW.append("\n");
-            StrW.write("Precision;Recall;F1;Iteração\n");
-        } catch (IOException ex) {
-            Logger.getLogger(ManipulaCSV.class.getName()).log(Level.SEVERE, null, ex);
+                    StrW.write("Precision;Recall;F1;Iteração\n");
+                    StrW.flush();
+                    StrW.close();
+
+                } catch (IOException ex) {
+                    System.out.println("Não foi possível escrever o cabeçalho no arquivo estatisticas.csv.");
+                }
+
+            } catch (IOException ex) {
+                System.out.println("Não foi possível criar arquivo estatisticas.csv.");
+            }
         }
 
     }
@@ -118,7 +122,7 @@ public class ManipulaCSV {
     public File padronizaCsvFile(File arquivo) throws IOException {
 
         File returnCSV = null;
-        
+
         try {
             BufferedReader arquivoCSV = new BufferedReader(new FileReader(arquivo.getPath()));
 
@@ -130,7 +134,7 @@ public class ManipulaCSV {
             String nome = arquivo.getName();
             nome = nome.substring(0, nome.indexOf('.'));
 
-            FileWriter newCSV = new FileWriter(diretorio + "\\" + nome + "_NEW.csv");
+            FileWriter newCSV = new FileWriter(diretorio + "\\" + nome + "_NEW.csv", true);
             returnCSV = new File(diretorio + "\\" + nome + "_NEW.csv");
 
             //Essa estrutura do looping while é clássica para ler cada linha
@@ -200,12 +204,13 @@ public class ManipulaCSV {
 
                 elemento1 = TableLine[0];
                 elemento2 = TableLine[1];
-                
+
                 System.out.println("e1: " + elemento1 + " e2: " + elemento2);
 
                 existe = buscaGabarito(elemento1, elemento2, gs);
 
                 if (existe) {
+//                    System.out.println("Existe! vp++");
                     vp++;
                 } else {
                     fp++;
@@ -224,7 +229,7 @@ public class ManipulaCSV {
     }
 
     private boolean buscaGabarito(String elemento1, String elemento2, File gs) {
-
+        //O gabarito tem de estar sem aspas
         String Str, elementoGS1, elementoGS2 = "";
         String[] TableLine;
         boolean existe = false;
@@ -238,10 +243,13 @@ public class ManipulaCSV {
 
                 elementoGS1 = TableLine[0];
                 elementoGS2 = TableLine[1];
-                
-                System.out.println("gs1: " + elementoGS1 + " gs2: " + elementoGS2);
 
+//                if(elemento1.equals("10230") && elemento2.equals("10383")){
+//                    System.out.println("gs1: " + elementoGS1 + " gs2: " + elementoGS2);
+//                }
+//                System.out.println("gs1: " + elementoGS1 + " gs2: " + elementoGS2);
                 if ((elemento1.equals(elementoGS1)) && (elemento2.equals(elementoGS2))) {
+//                    System.out.println("Existe em buscaGabarito.");
                     existe = true;
                     break;
                 }
@@ -271,7 +279,9 @@ public class ManipulaCSV {
             double f1 = getF1(precision, recall);
 
             try {
+                estatisticas = new FileWriter(estat, true);
                 BufferedWriter StrW = new BufferedWriter(estatisticas);
+//                System.out.println(estatisticas.);
 
 //                StrW.append(Double.toString(precision));
 //                StrW.append(";");
@@ -281,8 +291,12 @@ public class ManipulaCSV {
 //                StrW.append(";");
 //                StrW.append(Double.toString(iteracao));
 //                StrW.append("\n");
-
-            StrW.write(Double.toString(precision) + ";" + Double.toString(recall) + ";" + Double.toString(f1) + ";" + Double.toString(iteracao) + "\n");
+                StrW.write(Double.toString(precision) + ";" + Double.toString(recall) + ";" + Double.toString(f1) + ";" + Double.toString(iteracao) + "\n");
+//                estatisticas.flush();
+//                estatisticas.close();
+                StrW.flush();
+                StrW.close();
+                iteracao++;
 
             } catch (FileNotFoundException ex) {
                 Logger.getLogger(ManipulaCSV.class.getName()).log(Level.SEVERE, null, ex);

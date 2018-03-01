6bc4881d240035ac0998cff0455612b7c5805953
Diego <Diego@COMP-Diego>
2018-02-28 21:21:59 -0300

    Deduplicação da base de dados de CDs musicais: Resultados 19..23 OK

--- AS/src/AS/AplicacaoAS.java
+++ AS/src/AS/AplicacaoAS.java
@@ -28,6 +28,18 @@ public class AplicacaoAS {
         File resultado9 = new File("./src/csv/resultsDedup", "resultado9.csv");
         File resultado10 = new File("./src/csv/resultsDedup", "resultado10.csv");
         File resultado11 = new File("./src/csv/resultsDedup", "resultado11.csv");
+        File resultado12 = new File("./src/csv/resultsDedup", "resultado12.csv");
+        File resultado13 = new File("./src/csv/resultsDedup", "resultado13.csv");
+        File resultado14 = new File("./src/csv/resultsDedup", "resultado14.csv");
+        File resultado15 = new File("./src/csv/resultsDedup", "resultado15.csv");
+        File resultado16 = new File("./src/csv/resultsDedup", "resultado16.csv");
+        File resultado17 = new File("./src/csv/resultsDedup", "resultado17.csv");
+        File resultado18 = new File("./src/csv/resultsDedup", "resultado18.csv");
+        File resultado19 = new File("./src/csv/resultsDedup", "resultado19.csv");
+        File resultado20 = new File("./src/csv/resultsDedup", "resultado20.csv");
+        File resultado21 = new File("./src/csv/resultsDedup", "resultado21.csv");
+        File resultado22 = new File("./src/csv/resultsDedup", "resultado22.csv");
+        File resultado23 = new File("./src/csv/resultsDedup", "resultado23.csv");
 
         File gs = new File("./src/csv/datasets", "cd_gold.csv");
 
@@ -60,10 +72,34 @@ public class AplicacaoAS {
         obj.comparaConjuntos(obj.padronizaCsvFile(resultado9));
         obj.comparaConjuntos(obj.padronizaCsvFile(resultado10));
         obj.comparaConjuntos(obj.padronizaCsvFile(resultado11));
+        obj.comparaConjuntos(obj.padronizaCsvFile(resultado12));
+        obj.comparaConjuntos(obj.padronizaCsvFile(resultado13));
+        obj.comparaConjuntos(obj.padronizaCsvFile(resultado14));
+        obj.comparaConjuntos(obj.padronizaCsvFile(resultado15));
+        obj.comparaConjuntos(obj.padronizaCsvFile(resultado16));
+        obj.comparaConjuntos(obj.padronizaCsvFile(resultado17));
+        obj.comparaConjuntos(obj.padronizaCsvFile(resultado18));
+        obj.comparaConjuntos(obj.padronizaCsvFile(resultado19));
+        obj.comparaConjuntos(obj.padronizaCsvFile(resultado20));
+        obj.comparaConjuntos(obj.padronizaCsvFile(resultado21));
+        obj.comparaConjuntos(obj.padronizaCsvFile(resultado22));
+        obj.comparaConjuntos(obj.padronizaCsvFile(resultado23));
 
         obj.setPermutacao(1);
 
         obj.limpaTudo(); //Só utilizar após a execução de um conjunto de permutações
+        obj.comparaConjuntos(obj.padronizaCsvFile(resultado23));
+        obj.comparaConjuntos(obj.padronizaCsvFile(resultado22));
+        obj.comparaConjuntos(obj.padronizaCsvFile(resultado21));
+        obj.comparaConjuntos(obj.padronizaCsvFile(resultado20));
+        obj.comparaConjuntos(obj.padronizaCsvFile(resultado19));
+        obj.comparaConjuntos(obj.padronizaCsvFile(resultado18));
+        obj.comparaConjuntos(obj.padronizaCsvFile(resultado17));
+        obj.comparaConjuntos(obj.padronizaCsvFile(resultado16));
+        obj.comparaConjuntos(obj.padronizaCsvFile(resultado15));
+        obj.comparaConjuntos(obj.padronizaCsvFile(resultado14));
+        obj.comparaConjuntos(obj.padronizaCsvFile(resultado13));
+        obj.comparaConjuntos(obj.padronizaCsvFile(resultado12));
         obj.comparaConjuntos(obj.padronizaCsvFile(resultado11));
         obj.comparaConjuntos(obj.padronizaCsvFile(resultado10));
         obj.comparaConjuntos(obj.padronizaCsvFile(resultado9));
